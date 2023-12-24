33ece4b27472a339f4be9e453e928507997658db
aczyg <154689387+aczyg@users.noreply.github.com>
2023-12-24 22:19:00 +0800

    updates

--- script.js
+++ script.js
@@ -12,17 +12,19 @@ function opentab(tabname){
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabname).classList.add("active-tab");
 }
-/* projects */
-var projectstablinks = document.getElementsByClassName("projects-tab-links");
-var projectstabcontents = document.getElementsByClassName("projects-tab-contents");
 
-function opentab2(projectstabname){
-    for(projectstablink of projectstablinks){
-        projectstablink.classList.remove("active-link");
-    }
-    for(projectstabcontent of projectstabcontents){
-        projectstabcontent.classList.remove("active-tab");
-    }
-    event.currentTarget.classList.add("active-link");
-    document.getElementById(projectstabname).classList.add("active-tab");
-}
+document.getElementById("openLink1").addEventListener("click", function() {
+    window.open("https://xd.adobe.com/view/c9d0ab64-7538-4f9d-997c-7221e2ca895e-9c83/", "_blank");
+});
+
+document.getElementById("openLink2").addEventListener("click", function() {
+    window.open("https://xd.adobe.com/view/e0a06703-6b69-4ba4-9577-716122d6838e-b5f3/", "_blank");
+});
+
+document.getElementById("openLink3").addEventListener("click", function() {
+    window.open("https://xd.adobe.com/view/5c08ff33-8d80-43b7-acbf-f1f0f33580e9-095b/", "_blank");
+});
+
+document.getElementById("openLink4").addEventListener("click", function() {
+    window.open("https://aczyg.github.io/mech/", "_blank");
+});
