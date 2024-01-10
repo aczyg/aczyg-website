24dc8bce1f809cdb5545f909c021d962b6a3a9a3
aczyg <aczygx@gmail.com>
2024-01-10 20:39:22 +0800

    update

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 /* skills, experience, education */
 var tablinks = document.getElementsByClassName("tab-links");
 var tabcontents = document.getElementsByClassName("tab-contents");
+var sidemeu = document.getElementById("sidemenu");
 
 function opentab(tabname){
     for(tablink of tablinks){
@@ -13,18 +14,11 @@ function opentab(tabname){
     document.getElementById(tabname).classList.add("active-tab");
 }
 
-document.getElementById("openLink1").addEventListener("click", function() {
-    window.open("https://xd.adobe.com/view/c9d0ab64-7538-4f9d-997c-7221e2ca895e-9c83/", "_blank");
-});
+/* sidemenu */
+function openmenu(){
+    sidemeu.style.right = "0px";
+}
 
-document.getElementById("openLink2").addEventListener("click", function() {
-    window.open("https://xd.adobe.com/view/e0a06703-6b69-4ba4-9577-716122d6838e-b5f3/", "_blank");
-});
-
-document.getElementById("openLink3").addEventListener("click", function() {
-    window.open("https://xd.adobe.com/view/5c08ff33-8d80-43b7-acbf-f1f0f33580e9-095b/", "_blank");
-});
-
-document.getElementById("openLink4").addEventListener("click", function() {
-    window.open("https://aczyg.github.io/mech/", "_blank");
-});
+function closemenu(){
+    sidemeu.style.right = "-200px";
+}
